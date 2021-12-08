7782d3f680bb62bf976b6eac32e9897c2f3a83da
Dragos Iordache <dragosh.iordache@yahoo.com>
2021-12-08 21:11:33 +0200

    header footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,18 @@
+import Footer from '../components/shared/Footer';
+import Header from '../components/shared/Header';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <Header></Header>
+      </header>
+
+      <main></main>
+
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
 }
